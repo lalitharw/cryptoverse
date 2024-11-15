f65aaa1c655a4b42bef0973ae26d8870809914ef
lalitharwateappdid <lalitharwate.appdid@gmail.com>
2024-11-15 13:00:53 +0530

    updated spinner added

--- src/pages/CryptoCurrency.jsx
+++ src/pages/CryptoCurrency.jsx
@@ -1,50 +1,59 @@
-import { useGetCoinsQuery } from "../app/Crypto/CryptoApi"
-import { useState,useEffect } from "react"
+import { useGetCoinsQuery } from "../app/Crypto/CryptoApi";
+import { useState, useEffect } from "react";
 
 // react router dom
-import { Link } from "react-router-dom"
-import { CardComponent } from "../components/CardComponent"
+import { Link } from "react-router-dom";
+import { CardComponent } from "../components/CardComponent";
 
-export const CryptoCurrency = ({simplified}) => {
+export const CryptoCurrency = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
 
-    const count = simplified ? 10: 100
+  const { data, error, isLoading } = useGetCoinsQuery(count);
 
-    const{data,error,isLoading} = useGetCoinsQuery(count)
+  const [coins, setCoins] = useState(data?.data?.coins);
+  const [search, setSearch] = useState("");
 
-    const [coins,setCoins] = useState(data?.data?.coins)
-    const [search,setSearch] = useState("")
+  // console.log(coins)
 
-    // console.log(coins)
+  useEffect(() => {
+    // setCoins(data?.data?.coins)
+    const filterData = data?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setCoins(filterData);
+  }, [data, search]);
 
-    useEffect(() => {
-            // setCoins(data?.data?.coins)
-            const filterData = data?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(search.toLowerCase()))
-            setCoins(filterData)
-    },[data,search])
-
-    if(isLoading) return (<>
-    <p>Loading.. Please Wait</p>
-    </>)
-
+  if (isLoading)
     return (
-        <div className="container-fluid">
-        {
-            !simplified &&
-        <input type="text" className="form-control col-6" placeholder="Search CryptoCoin" name="" id="" onChange={(e) => setSearch(e.target.value)} />
-        }
-        <h2 className="fw-bold text-black">Crypto Currency</h2>
-        <div className="row">
+      <>
+        <p>Loading.. Please Wait</p>
+      </>
+    );
+
+  return (
+    <div>
+      {!simplified && (
+        <input
+          type="text"
+          className="form-control col-6"
+          placeholder="Search CryptoCoin"
+          name=""
+          id=""
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      )}
+      <h2 className="fw-bold text-black">Crypto Currency</h2>
+      <div className="row">
         {coins?.map((coin) => (
-            <>
-        <div className="col-lg-4">
-            <Link to={`crypto-details/${coin.uuid}`} >
-            {/* <p key={coin.uuid}>{coin.name} <img src={coin.iconUrl} width={30}/></p> */}
-            <CardComponent coin={coin}/>
-            </Link>
-        </div>
-            </>
+          <>
+            <div className="col-lg-4">
+              <Link className="text-decoration-none" to={`crypto-details/${coin.uuid}`}>
+                <CardComponent coin={coin} />
+              </Link>
+            </div>
+          </>
         ))}
-        </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,53 @@
-import { useGetCoinsStatsQuery } from "../app/Crypto/CryptoApi"
-import { useState } from "react"
+import { useGetCoinsStatsQuery } from "../app/Crypto/CryptoApi";
+import { useState } from "react";
 
-import millify from "millify"
+import millify from "millify";
+
+// react bootstrap
+import Spinner from 'react-bootstrap/Spinner';
 
 // components
-import { CryptoCurrency } from "./CryptoCurrency"
+import { CryptoCurrency } from "./CryptoCurrency";
 
-export const Home =() => {
+export const Home = () => {
+  const { data, error, isLoading } = useGetCoinsStatsQuery();
+  console.log(data?.data?.stats);
+  // const [globalStats,setGlobalStats] = useState(data?.data?.stats)
+  const globalStats = data?.data?.stats;
 
-    const {data, error, isLoading} = useGetCoinsStatsQuery()
-    console.log(data?.data?.stats)
-    // const [globalStats,setGlobalStats] = useState(data?.data?.stats)
-    const globalStats = data?.data?.stats
-
-    if(isLoading) return(
-        <>
-        <div>
-            Loading...
-        </div>
-        </>
-    )
-
+  if (isLoading)
     return (
-        <>
-        <p>Total Coins: {globalStats.totalCoins}</p>
-        <p>Total Exchanges: {millify(globalStats.totalExchanges)}</p>
-        <p>Total Markets: {millify(globalStats.totalMarkets)}</p>
-        <p>Total 24H Volume: {millify(globalStats.total24hVolume)}</p>
+      <>
+        <div className=" d-flex justify-content-center align-items-center " style={{height:"100vh"}}><Spinner/></div>
+      </>
+    );
 
-        <CryptoCurrency simplified={true}/>
-        </>
-    )
-}
+  return (
+    <div className="container-fluid bg-black-50">
+      <div className=" d-flex justify-content-center gap-5 p-5">
+        <div>
+          <span className="fs-5 fw-bold text-black">Total Coins:</span>
+          <br />
+          {globalStats.totalCoins}
+        </div>
+        <div>
+          <span className="fs-5 fw-bold text-black">Total Exchanges:</span>
+          <br />
+          {millify(globalStats.totalExchanges)}
+        </div>
+        <div>
+          <span className="fs-5 fw-bold text-black">Total Markets:</span>
+          <br />
+          {millify(globalStats.totalMarkets)}
+        </div>
+        <div>
+          <span className="fs-5 fw-bold text-black">Total 24h Volume:</span>
+          <br />
+          {millify(globalStats.total24hVolume)}
+        </div>
+      </div>
+
+      <CryptoCurrency simplified={true} />
+    </div>
+  );
+};
